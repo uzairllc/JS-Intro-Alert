f125b103c6ba3436eb11f38f0e10faf4b5a13093
Uzair Aziz Shakh <162738287+uzairllc@users.noreply.github.com>
2024-12-13 18:16:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+//popupalert//
+alert('Hello World');
+
+
+String => ' every characters except numbers';
+Number => '12345678910';
+
+
+
+alert(10 / 10);
+alert('Hello World');
+
+//alert paragraph//
+var paragraph = 'Hello World';
+alert(paragraph)
+
+// two var  popup//
+var number1 = 100;
+var number2 = 300;
+alert(number1 + number2);
+
+//alert greeting//
+var greeting = 'As salam O Alaikum ';
+var myName = 'MUHAMMAD UZAIR SHAIKH';
+
+alert(greeting + myName)
